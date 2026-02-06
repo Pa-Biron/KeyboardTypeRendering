d35a1fbf7c7f18d26171e383ce47d238975dfd75
Pierre-Alexandre Biron <pabiron@medyx.com>
2026-02-06 14:54:30 -0500

    Create base project

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Button, KeyboardTypeOptions, Text, TextInput, View } from "react-native";
+
+export default function Index() {
+  const [text, setText] = useState("");
+  const [keyboardType, setKeyboardType] = useState<KeyboardTypeOptions>("default");
+
+  const handleChangeKeyboardType = () => {
+    setKeyboardType(keyboardType === "default" ? "numeric" : "default");
+  };
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <TextInput placeholder="Add text." value={text} onChangeText={setText} keyboardType={keyboardType} />
+
+      <Button title="Change keyboard type" onPress={handleChangeKeyboardType} />
+    </View>
+  );
+}
